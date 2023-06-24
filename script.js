7be4655383897787b14c79a492bd17c4e23a285e
Collins CHARLES <31598209+El-Collins@users.noreply.github.com>
2023-06-24 22:51:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+ 'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+console.log(secretNumber);
+
+let score = 20;
+let highScore = 0;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number entered';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'You win!';
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector(".number").style.width = "30rem"
+    document.querySelector(".number").textContent = guess
+
+
+    if (score > highScore ){
+        highScore = score;
+        document.querySelector('.highscore').textContent = highScore
+    }
+  } 
+  
+
+  else if (guess !== secretNumber) {
+    if (score > 1) {
+    
+        document.querySelector('.message').textContent = guess > secretNumber ? 'Too High!' : "Too low!";
+        score--;
+        document.querySelector('.score').textContent = score;
+    
+        } else{
+            document.querySelector('.message').textContent = "You lose!"
+            document.querySelector('.score').textContent = 0
+    
+        }
+    
+  }
+  
+});
+
+
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+   secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector("body").style.backgroundColor = "#222"
+  document.querySelector(".number").style.width = "15rem"
+document.querySelector(".guess").value =""
+document.querySelector(".number").textContent= "?"
+
+
+
+});
